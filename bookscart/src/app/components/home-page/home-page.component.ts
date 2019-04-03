ef4f518a49b33bf2687f802a6b8d8cfbf0090062
Kumar Abhinav <abhinav0441@gmail.com>
2019-04-03 12:22:46 +0530

    template binding

--- bookscart/src/app/components/home-page/home-page.component.ts
+++ bookscart/src/app/components/home-page/home-page.component.ts
@@ -7,18 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  book: Book;
+  books: Book[];
 
   constructor() {
-    this.book = {
+    this.books = [
+      {
       title: 'The Alchemist',
       author: 'Paulo Cohelo',
       price: 23,
       rating: 4
-    };
+    },
+      {
+      title: 'Five point someone',
+      author: 'Chetan Bhagat',
+      price: 13,
+      rating: 1
+    },
+      {
+      title: 'The monk who sold his ferrari',
+      author: 'Robin Sharma',
+      price: 25,
+      rating: 3
+    },
+      {
+      title: 'Four hour work week',
+      author: 'Tim Ferris',
+      price: 27,
+      rating: 5
+    }
+  ]
   }
 
   ngOnInit() {
+  }
+
+  rateUp(book: Book) {
+    if(book.rating < 5)
+      book.rating++;
+  }
+
+  rateDown(book: Book) {
+    if(book.rating > 1)
+      book.rating--;
+  }
+
+  addBook(title, author, price, rating) {
+    let newBook : Book = {
+      title: title.value,
+      author: author.value,
+      price: +price.value,
+      rating: +rating.value
+    };
+    this.books.unshift(newBook);
   }
 
 }
