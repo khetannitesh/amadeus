60fa78735ca8cd484a3e9bf9f1b81bb589340e09
Kumar Abhinav <abhinav0441@gmail.com>
2019-04-04 10:23:37 +0530

    reactive forms

--- bookscart/src/app/components/add-book-page/add-book-page.component.ts
+++ bookscart/src/app/components/add-book-page/add-book-page.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { BookService } from './../../services/book.service';
 import { Book } from './../../models/interfaces';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-book-page',
@@ -9,25 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-book-page.component.css']
 })
 export class AddBookPageComponent implements OnInit {
+  bookForm = new FormGroup({
+    title: new FormControl(''),
+    author: new FormControl(''),
+    price: new FormControl(''),
+    rating: new FormControl(''),
+  });
 
   constructor(
     private bookService: BookService,
     private router: Router
-    ) {
-   }
+  ) {
+  }
+
+  save() {
+    console.log(this.bookForm.value);
+  }
 
   ngOnInit() {
   }
 
-  addBook(title, author, price, rating) {
-    let newBook : Book = {
-      title: title.value,
-      author: author.value,
-      price: +price.value,
-      rating: +rating.value
-    };
+  // updateName() {
+  //   this.name.setValue('Hello World');
+  // }
+
+  addBook() {
+    let newBook: Book = this.bookForm.value;
+    newBook.price = +newBook.price;
+    newBook.rating = +newBook.rating;
+
     this.bookService.addBook(newBook);
     this.router.navigate(['/home']);
   }
-  
+
 }
